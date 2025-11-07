e13e110df8d949c1f1d48855c3f2546ff53c902c
sahana160412 <sahanamurugesh16@gmail.com>
2025-11-07 18:48:57 +0530

    Add files via upload

---
+++ lesson 2/getting started.py
@@ -0,0 +1,10 @@
+# print a sentence
+print("welcome to the python programming!")
+# print a number
+print(5)
+# next line after print
+print("hello world\n")
+# printing multiple parameter
+print("hello",5)
+# ending with special symbol
+print("hurray",end="!")
---
+++ lesson 2/variables.py
@@ -0,0 +1,9 @@
+x = 7
+y = "sahana"
+print(x)
+print(y)
+Student_Name="kenya"
+print(Student_Name)
+# user input
+age = input("enter the age:")
+print("age of the user is", age ," years old")
