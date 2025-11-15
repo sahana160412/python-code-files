4eec35e8210302d3027085461f3196d23ebc2b9f
sahana160412 <sahanamurugesh16@gmail.com>
2025-11-15 21:15:59 +0530

    Add files via upload

---
+++ sorting bd.py/project.py
@@ -0,0 +1,10 @@
+# print a sentence
+print("it is my brithday!")
+# print a number of balloons needed
+print( 5)
+# next line after print
+print("hurray\n")
+# printing multiple parameter
+print("hello",5)
+# ending with special symbol
+print("hurray",end="!")
