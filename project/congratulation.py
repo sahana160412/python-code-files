40f3d204b87e2232b32890c123605141d69a6a9e
sahana160412 <sahanamurugesh16@gmail.com>
2025-11-24 14:48:40 +0530

    Add files via upload

---
+++ project/congratulation.py
@@ -0,0 +1,13 @@
+#operations on string
+#length of a string using len() function
+full_name = 'CONGRATULATION'
+print(len(full_name))
+#indexing - getting a single value from a string SYNTAX: str_name[index_value]
+print(full_name[4])
+#slicing - getting multiple values from a string SYNTAX: str_name[start: end +1]
+print(full_name[1:4])
+print(full_name[0:4])
+#concetanation - Joining 2 strings
+fn = 'Congratulations'
+ln = ' message '
+print(fn+ln)
