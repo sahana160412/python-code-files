85325f0829e075cea9e8877f9e6081042737fba2
sahana160412 <sahanamurugesh16@gmail.com>
2025-11-24 14:58:23 +0530

    Add files via upload

---
+++ project/calculate.py
@@ -0,0 +1,9 @@
+squareroot2= 1.414
+squareroot3= 1.732
+squareroot4= 2
+squareroot5= 2.336
+squareroot6= 2.449
+
+multiply = squareroot2*squareroot3*squareroot4*squareroot5*squareroot6
+print("multiply of all the 5 squarerooots" , multiply)
+
