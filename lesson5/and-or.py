26ad8bc447c545d00fc0e8cdd37daf6b461c15f1
sahana160412 <sahanamurugesh16@gmail.com>
2025-11-28 18:44:12 +0530

    Add files via upload

---
+++ lesson5/and-or.py
@@ -0,0 +1,17 @@
+a = 10
+b =12
+c = 0
+
+if a and b and c :
+    print("All number have boolean value as true")
+else:
+    print("Atleast one number has boolean value as false")
+
+a = 9
+b = -7
+c = 20
+
+if b<c or c<b :
+    print(" any one of the condition is true ")
+else:
+    print("any one of the condition is false")
---
+++ lesson5/not-equal.py
@@ -0,0 +1,20 @@
+a=10
+b=43
+c=43
+
+print( b != a)
+print( b != c)
+
+a= "apple"
+b= "orange"
+if a!=b :
+  print(a ,'and', b ,'are different.')
+
+a ="saha"
+b="saha"
+if a==b :
+  print(a ,'and', b,'are same.')
+
+
+
+
