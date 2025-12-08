e619fcb7336d44ce0183eef646d6cbe94e9eb116
sahana160412 <sahanamurugesh16@gmail.com>
2025-12-08 17:34:02 +0530

    Add files via upload

---
+++ project/check age.py
@@ -0,0 +1,10 @@
+check_age=input("did you have a age between 10  or  20:")
+atten=int(input("enter the age of your student:"))
+
+if check_age== '10':
+   print("you are allowed")
+else :
+    if atten <=20:
+        print("you are allowed ")
+    else:
+        print("not allowed")
