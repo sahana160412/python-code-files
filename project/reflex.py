7f9e5a08861b7b0bb210ed04ec906150ea6aa9e1
sahana160412 <sahanamurugesh16@gmail.com>
2025-12-23 20:51:47 +0530

    Add files via upload

---
+++ project/reflex.py
@@ -0,0 +1,7 @@
+print(' half pyramid of reflect stars')
+rows=int(input('enter the number of rows:'))
+for i in range(1,rows +1):
+    spaces=(rows-i)
+    stars=i
+    print(" " * spaces + "*" * stars)
+    print()
