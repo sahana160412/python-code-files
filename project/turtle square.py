855d9179402bd097b81e910c31f664001f2a56f3
sahana160412 <sahanamurugesh16@gmail.com>
2025-12-23 20:43:29 +0530

    Add files via upload

---
+++ project/turtle square.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.Screen().bgcolor('blue')
+board=turtle.Turtle()
+
+
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+
