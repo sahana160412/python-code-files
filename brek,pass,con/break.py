f27307f8c729de17f92cf5cf491872b14fd6f648
sahana160412 <sahanamurugesh16@gmail.com>
2026-01-05 05:50:15 -0800

    Add files via upload

---
+++ brek,pass,con/break.py
@@ -0,0 +1,7 @@
+a=input('Enter a word: ')
+for i in range(len(a)):
+ if a[i]=='a':
+  print('This character will be considered')
+  break
+
+print('This character will not be considered')
---
+++ brek,pass,con/break1.py
@@ -0,0 +1,7 @@
+a=input('Enter a word: ')
+for i in a:
+    if i=='a':
+        print(' character found')
+        break
+    else:
+        print(' character not found')
