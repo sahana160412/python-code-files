222ae791da61c16b8179a6aecb1df3ad66820660
sahana160412 <sahanamurugesh16@gmail.com>
2026-01-23 05:02:30 -0800

    Add files via upload

---
+++ DT.py
@@ -0,0 +1,9 @@
+from datetime import date ,time,datetime 
+today=date.today()
+now=datetime.now()
+
+print('Today date is', today)
+print('Today datetime is', now)
+
+print('Current date and time',now)
+print('Date Components',today.year,today.month,today.day)
