e4d8a131683bbf938d29f2a31f890f26eda08a1b
sahana160412 <sahanamurugesh16@gmail.com>
2026-01-28 02:24:54 -0800

    Add files via upload

---
+++ the month.py
@@ -0,0 +1,10 @@
+import calendar
+for i in range(1, 13):
+    print(calendar.month_name[i])
+
+months = ["January", "February", "March", "April", "May", "June", 
+          "July", "August", "September", "October", "November", "December"]
+
+for name in months:
+    print(name)
+
