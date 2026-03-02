a0698d5199748f38cb7e1413dfa726fd0dcf76f2
sahana160412 <sahanamurugesh16@gmail.com>
2026-03-02 04:14:54 -0800

    Add files via upload

---
+++ passwword.py
@@ -0,0 +1,7 @@
+import random
+import string
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+print(f"Your new password: {generate_password(16)}")
