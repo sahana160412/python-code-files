2f79a2fb8e70c749c719bb0238359260c2dafd69
sahana160412 <sahanamurugesh16@gmail.com>
2026-03-02 05:43:13 -0800

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,10 @@
+'''Write a program to create a class with the name Student and perform the following tasks - 
+1. Declare a variable grade
+2. Print a sentence inside the class 
+3. Create an object of class student and see the output'''
+
+class students:
+    grade=9
+    def __init__(self):
+        print('I am inside the init method')
+s1=students()
