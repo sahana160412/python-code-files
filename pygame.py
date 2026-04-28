04334554468d7152d643035e7fa1b3f3caf260b7
sahana160412 <sahanamurugesh16@gmail.com>
2026-04-28 20:18:24 +0530

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("My First Game Screen")
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 255))
+    pygame.display.update()
+pygame.quit()
